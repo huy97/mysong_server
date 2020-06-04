f471466be6d3d25c10e3fbd7f38134bc618cd4b1
Huy Le <huylt@dtsgroup.com.vn>
2020-06-04 08:25:53 +0700

    Update

--- middleware/maintenanceMode.js
+++ middleware/maintenanceMode.js
@@ -3,7 +3,7 @@ const {defaultResponse} = require('../utils/helper');
 const maintenanceMode = (isMaintain) => {
     return (req, res, next) => {
         if (isMaintain === 'maintenance') {
-            return defaultResponse(res, 500, 'Bảo trì hệ thống');
+            return defaultResponse(res, 500, 'Hệ thống đang tiến hành bảo trì và nâng cấp.');
         }
         next();
     }
